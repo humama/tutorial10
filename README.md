9769bc806be9c3e38d4540c7899fac309bc280ed
Humam Al Labib <humam.alabib@gmail.com>
2024-05-08 15:44:13 +0700

    Experiment 1.2: Understanding how it works.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Experiment 1.2
+# Experiment 1.2
+
 ![image](https://github.com/humama/tutorial10/assets/20278539/5a1d1a64-6c50-442d-95d5-f1f1228f1c70)
 Yang dijalankan adalah yang "hey hey" dulu karena perintah println untuk "hey hey" dijalankan duluan daripada spawner atau executor
--- src/main.rs
+++ src/main.rs
@@ -99,6 +99,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Komputer: done!");
     });
+    println!("Komputer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
